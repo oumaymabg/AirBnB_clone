ca5e77cb14870fd246b5ea63fd5b24037a4ea745
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-11-03 17:52:27 +0000

    shit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """storage class
 """
+
+
 import json
 import os
 
@@ -8,8 +10,10 @@ import os
 class FileStorage:
     """ storage Object
     """
-    __file_path = "file.json"
-    __objects = {}
+    def __init__(self, *args, **kwargs):
+        self.__file_path = "file.json"
+        open(self.__file_path, 'a')
+        self.__objects = {}
 
     def all(self):
         """ holder """
@@ -18,7 +22,7 @@ class FileStorage:
     def new(self, obj):
         """ holder """
         self.__objects[obj.__class__.__name__ + "." + obj.id] = obj.to_dict()
-
+ 
     def save(self):
         """ holder """
         with open(self.__file_path, 'w') as f:
@@ -28,5 +32,5 @@ class FileStorage:
     def reload(self):
         """ holder """
         if(os.stat(self.__file_path).st_size is not 0):
-            with open(self.__file_path) as json_file:
-                self.__objects = json.load(json_file)
+            with open(self.__file_path) as json_file: 
+                self.__objects = json.load(json_file) 
