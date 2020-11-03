2ade90ef6de2d04873ebff03ad4237138b00ff6c
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-11-03 13:22:46 +0000

    message fix

--- console.py
+++ console.py
@@ -1,15 +1,117 @@
 #!/usr/bin/python3
 import cmd
 import sys
+import json
+from models.engine.file_storage import FileStorage
+
+class idClasses:
+    from models.base_model import BaseModel
+    from models.engine.file_storage import FileStorage
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
     def do_quit(self, arg):
-        "Quit command to exit the program"
+        "Quit command to exit the program\n"
         exit()
+
     def do_EOF(self, arg):
-        "Quit the program when EOF"
+        "Quit the program when EOF\n"
         exit()
+
+    def do_create(self, arg):
+        if(arg == ""):
+            print("** class name missing **")
+        elif(not hasattr(idClasses, arg)):
+            print("** class doesn't exist **")
+        else:
+            targetClass = getattr(idClasses, arg)
+            new = targetClass()
+            new.save()
+            print(new.id)
+
+    def do_show(self, arg):
+        f = FileStorage()
+        f.reload()
+        objs = f.all()
+        args = arg.split()
+        if(arg == ""):
+            print("** class name missing **")
+        elif (not hasattr(idClasses, args[0])):
+            print("** class doesn't exist **")
+        elif (len(args) < 2):
+            print("** instance id missing **")
+        else:
+            obj = ['','']
+            for key in objs:
+                obj = [objs[key]['class'], objs[key]['id']]
+                if obj == args:
+                    print("[{}] ({}) {}".format(objs[key]['class'], objs[key]['id'], objs[key]))
+                    return
+            print("** no instance found **")
+
+    def do_destroy(self, arg):
+        f = FileStorage()
+        f.reload()
+        objs = f.all()
+        args = arg.split()
+        if(arg == ""):
+            print("** class name missing **")
+        elif(not hasattr(idClasses, args[0])):
+            print("** class doesn't exist **")
+        elif (len(args) < 2):
+            print("** instance id missing **")
+        else:
+            obj = ['','']
+            for key in objs:
+                obj = [objs[key]['class'], objs[key]['id']]
+                if obj == args:
+                    del objs[key]
+                    f.save()
+                    return
+            print("** no instance found **")
+
+    def do_all(self, arg):
+        f = FileStorage()
+        f.reload()
+        objs = f.all()
+        ls = []
+        if arg == "":
+            for key in objs:
+                string = "[{}] ({}) {}".format(objs[key]['class'], objs[key]['id'], objs[key])
+                ls.append(string)
+            print(ls)
+        elif(not hasattr(idClasses, arg)):
+            print("** class doesn't exist **")
+        else:
+            for key in objs:
+                cl = key.split(".")
+                if cl[0] == arg:
+                    string = "[{}] ({}) {}".format(objs[key]['class'], objs[key]['id'], objs[key])
+                    ls.append(string)
+            print(ls)
+
+    def do_update(self, arg):
+        f = FileStorage()
+        f.reload()
+        objs = f.all()
+        args = arg.split()
+        if(arg == ""):
+            print("** class name missing **")
+        elif(not hasattr(idClasses, args[0])):
+            print("** class doesn't exist **")
+        elif (len(args) < 2):
+            print("** instance id missing **")
+        elif (len(args) < 3):
+            print("** attribute name missing **")
+        elif (len(args) < 4):
+            print("** value missing **")
+        else:
+            for key in objs:
+                obj = key.split(".")
+                if obj[0] == args[0] and obj[1] == args[1]:
+                    objs[key][args[2]] = args[3]
+                    f.save()
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
