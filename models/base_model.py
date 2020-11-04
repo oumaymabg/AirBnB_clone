5ae0eac938e894595be14d99f1123d3f27e899eb
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2020-11-04 10:24:26 +0000

    testing

--- models/base_model.py
+++ models/base_model.py
@@ -37,6 +37,7 @@ class BaseModel:
         """ holder """
         new = self.__dict__.copy()
         new['__class__'] = self.__class__.__name__
-        new['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        new['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        if not(type(new['updated_at']) is str):
+            new['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+            new['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         return new
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """storage class
 """
-
-
 import json
 import os
-
+from models.base_model import BaseModel
 
 class FileStorage:
     """ storage Object
@@ -21,16 +19,24 @@ class FileStorage:
 
     def new(self, obj):
         """ holder """
-        self.__objects[obj.__class__.__name__ + "." + obj.id] = obj.to_dict()
+        self.__objects[obj.__class__.__name__ + "." + obj.id] = obj
  
     def save(self):
         """ holder """
+        dic = {}
+        for key, value in self.__objects.items():
+            dic[key] = value.to_dict()
         with open(self.__file_path, 'w') as f:
-            json.dump(self.__objects, f)
+            json.dump(dic, f)
         f.close()
 
     def reload(self):
         """ holder """
+        idclasses = {'BaseModel': BaseModel}
+        data = {}
         if(os.stat(self.__file_path).st_size is not 0):
-            with open(self.__file_path) as json_file: 
-                self.__objects = json.load(json_file) 
+            with open(self.__file_path) as json_file:
+                data = json.load(json_file) 
+                for key, value in data.items():
+                    self.__objects[key] = idclasses[value['__class__']](**value)
+                
